cab755703e8f5deaa2459b5308c62baea543978f
friendzis <friendzis@gmail.com>
2022-12-02 12:02:02 +0200

    day02a

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -1,3 +1,86 @@
+use std::str::FromStr;
+
+#[derive(Debug)]
+enum HandSign {
+    ROCK,
+    PAPER,
+    SCISSORS,
+}
+
+impl FromStr for HandSign {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(HandSign::ROCK),
+            "Y" => Ok(HandSign::PAPER),
+            "Z" => Ok(HandSign::SCISSORS),
+            "A" => Ok(HandSign::ROCK),
+            "B" => Ok(HandSign::PAPER),
+            "C" => Ok(HandSign::SCISSORS),
+            _   => Err(()),
+        }
+    }
+}
+
+enum RoundOutcome {
+    WIN,
+    LOSS,
+    DRAW
+}
+
+fn round_outcome(theirs: &HandSign, ours: &HandSign) -> RoundOutcome {
+    match ours {
+        HandSign::ROCK => match theirs {
+            HandSign::ROCK      => RoundOutcome::DRAW,
+            HandSign::PAPER     => RoundOutcome::LOSS,
+            HandSign::SCISSORS  => RoundOutcome::WIN,
+        },
+        HandSign::PAPER => match theirs {
+            HandSign::ROCK      => RoundOutcome::WIN,
+            HandSign::PAPER     => RoundOutcome::DRAW,
+            HandSign::SCISSORS  => RoundOutcome::LOSS,
+        },
+        HandSign::SCISSORS => match theirs {
+            HandSign::ROCK      => RoundOutcome::LOSS,
+            HandSign::PAPER     => RoundOutcome::WIN,
+            HandSign::SCISSORS  => RoundOutcome::DRAW,
+        },
+    }
+}
+
+fn round_score(theirs: &HandSign, ours: &HandSign) -> i32 {
+    let hand_score = match ours {
+        HandSign::ROCK      => 1,
+        HandSign::PAPER     => 2,
+        HandSign::SCISSORS  => 3
+    };
+    let match_score = match round_outcome(theirs, ours) {
+        RoundOutcome::WIN   =>  6,
+        RoundOutcome::DRAW  =>  3,
+        RoundOutcome::LOSS  =>  0
+    };
+    hand_score + match_score
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../input");
+    let plays: _ = 
+    input
+        .split("\n")
+        .map(|play| {
+            play.split(" ")
+            .map(|item| {
+                HandSign::from_str(item).unwrap()
+            })
+            .collect::<Vec<HandSign>>()
+        })
+        .map(|play| round_score(&play[0], &play[1]))
+        .sum::<i32>()
+        // .fold(0, |acc, el: Vec<HandSign>| acc + roundScore(el[0], el[1]))
+        ;
+    // let plays = input.split("\n");
+    println!("{:?}", plays);
+    // foo("a");
+    // println!("Hello, world!");
 }
